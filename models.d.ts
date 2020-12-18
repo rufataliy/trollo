c3b298cfc726fe52628d5701d81524d776c00e00
Rufat Aliyev <46456022+rufataliy@users.noreply.github.com>
2020-12-18 17:04:06 -0500

    Added models

---
+++ models.d.ts
@@ -0,0 +1,15 @@
+import { Interface } from "readline";
+
+interface Card {
+  id: string;
+  type?: string;
+  content?: string;
+  board_id: string;
+  title: string;
+  deadline?: string;
+}
+
+interface Board {
+  id: string;
+  title: string;
+}
