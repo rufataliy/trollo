6f2a512be162e4ab8f10de99ee2508b67aadb1bb
Rufat Aliyev <46456022+rufataliy@users.noreply.github.com>
2020-12-19 00:25:09 -0500

    Added drag and drop without logic

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,62 @@
 import Head from "next/head";
 import React from "react";
+import { Draggable, Droppable } from "react-beautiful-dnd";
 import { Container, Card, NewBoard } from "../components";
+import { DragDropContext } from "react-beautiful-dnd";
 import { useStore } from "../store";
 
 export default function Home() {
   const { boards, cards } = useStore();
+
   return (
     <>
       <Head>
         <title>Trollo | Boards</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="main-content p-3">
-        {boards.map((board) => {
-          return (
-            <Container board={board}>
-              {cards.map((card) => {
-                if (card.board_id === board.id) return <Card card={card} />;
-              })}
-            </Container>
-          );
-        })}
-        <NewBoard />
-      </div>
+      <DragDropContext onDragEnd={() => {}}>
+        <div className="main-content p-3">
+          {boards.map((board) => {
+            return (
+              <Container board={board}>
+                <Droppable key={board.id} droppableId={board.id}>
+                  {(provided) => {
+                    return (
+                      <div {...provided.droppableProps} ref={provided.innerRef}>
+                        {cards.map((card, index) => {
+                          if (card.board_id === board.id) {
+                            return (
+                              <Draggable
+                                draggableId={card.id}
+                                index={index}
+                                key={card.id}
+                              >
+                                {(provided) => {
+                                  return (
+                                    <div
+                                      {...provided.draggableProps}
+                                      {...provided.dragHandleProps}
+                                      ref={provided.innerRef}
+                                    >
+                                      <Card card={card} />
+                                    </div>
+                                  );
+                                }}
+                              </Draggable>
+                            );
+                          }
+                        })}
+                        {provided.placeholder}
+                      </div>
+                    );
+                  }}
+                </Droppable>
+              </Container>
+            );
+          })}
+          <NewBoard />
+        </div>
+      </DragDropContext>
     </>
   );
 }
