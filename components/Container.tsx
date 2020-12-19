7dbe0e146f7f7e72bd2ef272e59e89230a69433f
Rufat Aliyev <46456022+rufataliy@users.noreply.github.com>
2020-12-19 13:28:06 -0500

    Added EditBoard component

--- components/Container.tsx
+++ components/Container.tsx
@@ -4,6 +4,7 @@ import { useOutsideClick } from "../customHooks";
 import { ConfirmPopover } from "./ConfirmPopover";
 import { DropDown } from "./DropDown";
 import { NewCard } from "./NewCard";
+import { EditBoard } from "./EditBoard";
 
 interface Props {
   board: Board;
@@ -11,8 +12,10 @@ interface Props {
 
 export const Container: React.FC<Props> = ({ children, board }) => {
   const [showPopover, setShowPopover] = useState(false);
+  const [editing, setEditing] = useState(false);
 
   const refPopover = useRef<HTMLDivElement>(null);
+  const refHeader = useRef<HTMLDivElement>(null);
 
   const handleDelete = (board: Board) => {
     console.log("delete");
@@ -23,46 +26,64 @@ export const Container: React.FC<Props> = ({ children, board }) => {
     console.log("reset");
     setShowPopover(false);
   };
+  const resetEditing = () => {
+    setEditing(false);
+  };
 
   useOutsideClick(refPopover, resetPopover);
+  useOutsideClick(refHeader, resetEditing);
 
   return (
     <div className="card-container mr-3">
-      <div className="d-flex justify-content-between card-container-header p-2">
-        <h5 className="m-0">{board.title}</h5>
-        <DropDown id={board.id} reset={() => console.log()}>
-          <div ref={refPopover}>
-            <ConfirmPopover
-              id={board.id}
-              show={showPopover}
-              text={"Confirm delete."}
-              Target={
-                <Button
-                  onClick={(e) => {
-                    e.stopPropagation();
-                    setShowPopover(true);
-                  }}
-                  className="btn-sm text-secondary d-flex align-items-center text-left btn-light w-100"
-                >
-                  <p className="m-0 mr-1">Delete</p>
-                  <i className="bi bi-trash-fill mt-n1" />
-                </Button>
-              }
-              onCancel={() => setShowPopover(false)}
-              onConfirm={() => handleDelete(board)}
-            />
-          </div>
+      <div
+        ref={refHeader}
+        className="d-flex justify-content-between card-container-header p-2"
+      >
+        {editing ? (
+          <EditBoard
+            handleSubmit={() => console.log()}
+            handleCancel={resetEditing}
+            board={board}
+          />
+        ) : (
+          <>
+            <h5 className="m-0">{board.title}</h5>
+            <DropDown id={board.id} reset={() => console.log()}>
+              <div ref={refPopover}>
+                <ConfirmPopover
+                  id={board.id}
+                  show={showPopover}
+                  text={"Confirm delete."}
+                  Target={
+                    <Button
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        setShowPopover(true);
+                      }}
+                      className="btn-sm text-secondary d-flex align-items-center text-left btn-light w-100"
+                    >
+                      <p className="m-0 mr-1">Delete</p>
+                      <i className="bi bi-trash-fill mt-n1" />
+                    </Button>
+                  }
+                  onCancel={() => setShowPopover(false)}
+                  onConfirm={() => handleDelete(board)}
+                />
+              </div>
 
-          <Button
-            onClick={(e) => {
-              e.stopPropagation();
-            }}
-            className="btn-sm text-secondary d-flex align-items-center text-left btn-light w-100"
-          >
-            <p className="m-0 mr-1">Edit</p>
-            <i className="bi bi-pencil-square mt-n1"></i>
-          </Button>
-        </DropDown>
+              <Button
+                onClick={(e) => {
+                  e.stopPropagation();
+                  setEditing(true);
+                }}
+                className="btn-sm text-secondary d-flex align-items-center text-left btn-light w-100"
+              >
+                <p className="m-0 mr-1">Edit</p>
+                <i className="bi bi-pencil-square mt-n1"></i>
+              </Button>
+            </DropDown>
+          </>
+        )}
       </div>
       <div className="card-container-list p-2">{children}</div>
       <div className="card-container-footer p-2">
