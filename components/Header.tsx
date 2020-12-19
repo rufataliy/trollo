1520b0cc731ac3b470dffc05c2510f277175fce0
Rufat Aliyev <46456022+rufataliy@users.noreply.github.com>
2020-12-19 16:09:46 -0500

    Added icons and nav links to the header

--- components/Header.tsx
+++ components/Header.tsx
@@ -15,19 +15,15 @@ export const Header = () => {
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#link">Link</Nav.Link>
-          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">
-              Another action
-            </NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">
-              Separated link
-            </NavDropdown.Item>
-          </NavDropdown>
+          <Nav.Link href="/">
+            Home <i className="bi bi-house-door-fill ml-1"></i>
+          </Nav.Link>
+          <Nav.Link href="/boards">
+            Boards <i className="bi bi-columns-gap ml-1"></i>
+          </Nav.Link>
+          <Nav.Link href="/calendar">
+            Calendar<i className="bi bi-calendar-range-fill ml-1"></i>
+          </Nav.Link>
         </Nav>
         <Form inline>
           <FormControl type="text" placeholder="Search" className="mr-sm-2" />
