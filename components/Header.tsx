857800e12eb1a718a0489a3c9f9b05f8e8276eab
Rufat Aliyev <46456022+rufataliy@users.noreply.github.com>
2020-12-19 22:52:02 -0500

    Added organized navigation

--- components/Header.tsx
+++ components/Header.tsx
@@ -11,18 +11,30 @@ import {
 export const Header = () => {
   return (
     <Navbar variant="dark" expand="lg">
-      <Navbar.Brand href="#home">Trollo</Navbar.Brand>
+      <Navbar.Brand className="d-flex align-items-center" href="/">
+        <img
+          src="/logo.svg"
+          width="30"
+          height="20"
+          className="d-inline-block align-top"
+          alt="Trollo logo"
+        />{" "}
+        Trollo
+      </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link href="/">
-            Home <i className="bi bi-house-door-fill ml-1"></i>
+          <Nav.Link className="d-flex align-items-center" href="/">
+            <i className="bi bi-house-door-fill mr-1 mt-n1"></i>
+            <span>Home</span>
           </Nav.Link>
-          <Nav.Link href="/boards">
-            Boards <i className="bi bi-columns-gap ml-1"></i>
+          <Nav.Link className="d-flex align-items-center" href="/boards">
+            <i className="bi bi-columns-gap mr-1 mt-n1"></i>
+            <span>Boards</span>
           </Nav.Link>
-          <Nav.Link href="/calendar">
-            Calendar<i className="bi bi-calendar-range-fill ml-1"></i>
+          <Nav.Link className="d-flex align-items-center" href="/calendar">
+            <i className="bi bi-calendar-range-fill mr-1 mt-n1"></i>
+            <span>Calendar</span>
           </Nav.Link>
         </Nav>
         <Form inline>
