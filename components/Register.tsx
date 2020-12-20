6d763cc0731964bfd1062d164ba926c56322aa1b
Rufat Aliyev <46456022+rufataliy@users.noreply.github.com>
2020-12-19 22:58:07 -0500

    Created register form

--- components/Register.tsx
+++ components/Register.tsx
@@ -1,12 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, Button } from "react-bootstrap";
 
-export const Register = () => {
+const isEmptyString = (str: string) => !Boolean(str.trim());
+
+interface Props {
+  handleSubmit: (values: DefaultRegisterValues) => void;
+}
+
+const defaultValues: DefaultRegisterValues = {
+  trollo_name: "",
+  trollo_company: "",
+};
+
+const defaultErrors = { name: false, company: false };
+
+export const Register: React.FC<Props> = ({ handleSubmit }) => {
+  const [values, setValues] = useState(defaultValues);
+  const [errors, setErrors] = useState(defaultErrors);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (
+      isEmptyString(values.trollo_company) ||
+      isEmptyString(values.trollo_name)
+    ) {
+      return setErrors((errors) => ({
+        ...errors,
+        company: isEmptyString(values.trollo_company),
+        name: isEmptyString(values.trollo_name),
+      }));
+    }
+
+    handleSubmit(values);
+  };
+
+  const onReset = () => {
+    setErrors({ ...defaultErrors });
+    setValues({ ...defaultValues });
+  };
+
   return (
-    <Form className="register-form text-light d-flex flex-column justify-content-between bg-dark-op p-3 p-lg-5">
+    <Form
+      onReset={onReset}
+      onSubmit={onSubmit}
+      className="register-form text-light d-flex flex-column justify-content-between bg-dark-op p-3 p-lg-5"
+    >
       <Form.Group controlId="formBasicEmail">
         <Form.Label>Name</Form.Label>
-        <Form.Control type="email" placeholder="Enter your name" />
+        <Form.Control
+          type="text"
+          placeholder="Enter your name"
+          isInvalid={errors.name}
+          onChange={(e) =>
+            setValues((values) => ({
+              ...values,
+              trollo_name: e.target.value,
+            }))
+          }
+        />
         <Form.Text className="text-light">
           Nothing will be persisted or saved
         </Form.Text>
@@ -14,7 +65,17 @@ export const Register = () => {
 
       <Form.Group controlId="formBasicPassword">
         <Form.Label>Company</Form.Label>
-        <Form.Control type="text" placeholder="Your organization" />
+        <Form.Control
+          type="text"
+          placeholder="Your organization"
+          isInvalid={errors.company}
+          onChange={(e) =>
+            setValues((values) => ({
+              ...values,
+              trollo_company: e.target.value,
+            }))
+          }
+        />
       </Form.Group>
       <div className="d-flex">
         <Button
@@ -25,7 +86,7 @@ export const Register = () => {
           Create
         </Button>
 
-        <Button className="w-100 btn-secondary" variant="primary" type="submit">
+        <Button className="w-100 btn-secondary" variant="primary" type="reset">
           Reset
         </Button>
       </div>
