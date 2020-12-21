355d617e5f8db809d45b0d3ebb8f1b1121be2219
Rufat Aliyev <46456022+rufataliy@users.noreply.github.com>
2020-12-20 20:31:06 -0500

    Added calendar page

---
+++ pages/calendar.tsx
@@ -0,0 +1,29 @@
+import Head from "next/head";
+import React, { useEffect } from "react";
+import { Header } from "../components";
+import { checkRegisteration } from "../utils";
+import Router from "next/router";
+import { Calendar } from "../components/Calendar";
+
+export default function Boards() {
+  useEffect(() => {
+    if (!checkRegisteration()) {
+      Router.push("/");
+    }
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <title>Trollo | Boards</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="wrapper">
+        <Header />
+        <main className="p-3">
+          <Calendar />
+        </main>
+      </div>
+    </>
+  );
+}
