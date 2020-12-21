06df2730cc82d9c094d398f0d303ba16cb8ffad4
Rufat Aliyev <46456022+rufataliy@users.noreply.github.com>
2020-12-21 17:40:45 -0500

    Added custom day cell render hook

--- components/Calendar.tsx
+++ components/Calendar.tsx
@@ -1,10 +1,13 @@
 import React, { useRef, useState } from "react";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
+import fullCalendarInteraction from "@fullcalendar/interaction";
 import { useStore } from "../store";
 import { Card } from "./Card";
 import { useOutsideClick } from "../customHooks";
 import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import { NewCard } from "./NewCard";
+import { NewBoard } from "./NewBoard";
 
 const positionsDefault = {
   clientX: null,
@@ -21,7 +24,7 @@ export const Calendar = () => {
   const [{ clientX, clientY }, setCardPosition] = useState(positionsDefault);
   const ref = useRef<HTMLDivElement>(null);
 
-  const registerEventClick = (id: string, clientX: number, clientY: number) => {
+  const registerDateClick = (id: string, clientX: number, clientY: number) => {
     setCardPosition(() => ({
       clientX,
       clientY,
@@ -39,7 +42,7 @@ export const Calendar = () => {
     jsEvent: { clientX, clientY },
   }) => {
     if (!selectedCard || selectedCard.id !== id) {
-      registerEventClick(id, clientX, clientY);
+      registerDateClick(id, clientX, clientY);
     } else {
       unregisterEventClick();
     }
@@ -83,12 +86,41 @@ export const Calendar = () => {
       </OverlayTrigger>
     );
   };
+
+  const renderCustomDay = (e) => {
+    const dateString = e.date.toDateString();
+    const localString = e.date.toLocaleDateString("en-CA");
+    return (
+      <OverlayTrigger
+        trigger="click"
+        placement="right"
+        rootClose
+        flip
+        overlay={
+          <div className="calendar-card p-1 pb-0">
+            <p className=" text-light calendar-card-add-title p-2 m-0">
+              {dateString}
+            </p>
+            <NewCard date={localString} />
+            <NewBoard />
+          </div>
+        }
+      >
+        <div className="d-flex align-items-center justify-content-between">
+          <span>add</span>
+          <span>{e.dayNumberText}</span>
+        </div>
+      </OverlayTrigger>
+    );
+  };
+
   useOutsideClick(ref, unregisterEventClick);
   return (
     <div ref={ref} className="calendar-wrapper p-3">
       <FullCalendar
         eventClick={handleEventClick}
         eventContent={renderCustomEvent}
+        dayCellContent={renderCustomDay}
         events={cards}
         plugins={[dayGridPlugin, fullCalendarInteraction]}
         initialView="dayGridMonth"
