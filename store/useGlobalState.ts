145f597048ba332e9a10fb14bc754b6e169148bf
Rufat Aliyev <46456022+rufataliy@users.noreply.github.com>
2020-12-21 20:53:29 -0500

    Added alert methods to the global state

--- store/useGlobalState.ts
+++ store/useGlobalState.ts
@@ -5,6 +5,7 @@ export const useGlobalState = (): ContextDefault => {
   const [cards, setCards] = useState<Card[] | null>(cardsDefault);
   const [boards, setBoards] = useState<Board[] | null>(boardsDefault);
   const [selectedCard, setSelectedCard] = useState<Card | null>(null);
+  const [alert, setAlert] = useState<Alert | null>(null);
 
   const addNewBoard = (newBoard: Board) => {
     setBoards((boards) => {
@@ -86,6 +87,14 @@ export const useGlobalState = (): ContextDefault => {
 
   const resetCalendarEvent = () => setSelectedCard(null);
 
+  const showAlert = (alert: Alert) => {
+    setAlert(() => alert);
+  };
+
+  const resetAlert = () => {
+    setAlert(null);
+  };
+
   return {
     cards,
     boards,
@@ -100,5 +109,8 @@ export const useGlobalState = (): ContextDefault => {
     selectedCard,
     saveCalendarEvent,
     resetCalendarEvent,
+    alert,
+    resetAlert,
+    showAlert,
   };
 };
